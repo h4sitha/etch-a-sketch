519c5768dcc45754513c97c4ef6fd1b5151abbf6
Hasitha Ekanayake <150508298+h4sitha@users.noreply.github.com>
2024-02-29 13:38:05 +0530

    Add button to ask for square number in the grid

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#container");
+const changeSquare = document.querySelector("button");
 
 for (let i=0; i < 256; i++) {
     const colorSquare = document.createElement("div");
@@ -13,5 +14,13 @@ colorSquare.forEach((singleSquare) => {
     singleSquare.addEventListener("mouseover", (e) => {
         e.target.style.backgroundColor = "black";
     })
+
+})
+
+
+
+changeSquare.addEventListener("click", () => {
+
+    const squareNumber = prompt("How many squares you want in each row?");
 
 })
